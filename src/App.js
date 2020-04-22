3327e69298c542e1be826b05ac0f740e02295409
Christian Holman <chrisholm2@yahoo.com>
2020-04-22 20:38:34 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  var [accountId, setAccountId] = useState("");
+  var [currentAccountId, setCurrentAccountId] = useState("");
+  var [topPayments, setTopPayments] = useState([]);
+
+  var getAccountTransactions = () => {
+    var headers = {
+      "x-api-key": "YOUR DRAGONGLASS API KEY",
+    };
+
+    fetch(`https://api.dragonglass.me/hedera/api/accounts/${accountId}/transactions?size=1000`, { headers })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          var accounts = {};
+          // For every data entry in the response (holds transaction details)
+          result["data"].forEach((data) => {
+            // For every transfer in the transaction (includes what accounts were credited and debited)
+            data["transfers"].forEach((transfer) => {
+              // Make sure that we only include accounts that are not our own, that were debited during transaction.
+              if (transfer["accountID"] !== accountId && transfer["amount"] > 0) {
+                if(!(transfer["accountID"] in accounts)) {
+                  accounts[transfer["accountID"]] = 0;
+                }
+                accounts[transfer["accountID"]] += transfer["amount"];
+              }
+            })
+          });
+
+          var sortedKeys = Object.keys(accounts);
+          sortedKeys.sort((a, b) => accounts[a] - accounts[b]).reverse()
+
+          var sortedAccounts = sortedKeys.map(key => {
+            return {
+              accountId: key,
+              amount: accounts[key],
+            }
+          });
+
+          sortedAccounts = sortedAccounts.filter((account) => account.amount > 0);
+
+          setTopPayments(sortedAccounts);
+          setCurrentAccountId(accountId);
+        },
+        (error) => {
+          console.error(error)
+        }
+      );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container">
+        <h1 className="page-header">
+          Where'd my hbar go?
+        </h1>
+        <form 
+          id="search-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            getAccountTransactions();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input 
+            placeholder="Enter an account ID e.g. 0.0.1234" 
+            onChange={(e) => {
+              setAccountId(e.target.value);
+            }}
+          />
+          <button type="submit">
+            View
+          </button>
+        </form>
+        {
+          currentAccountId !== "" ?
+          <div className="leaderboard">
+            <div className="leaderboard-header">
+              <h2 className="leaderboard-title">
+                Who has account {currentAccountId} sent the most ℏ to?
+              </h2>
+            </div>
+            <div className="leaderboard-body">
+              {
+                topPayments.map(
+                  (account, index) => (
+                    <div className="leaderboard-entry">
+                      <span className="ranking-number">{index + 1}. </span> 
+                      <span className="ranking-account">{account.accountId}</span> 
+                      <br />
+                      <span className="ranking-amount"> {(account.amount / 100000000).toFixed(2)} ℏ</span>
+                    </div>
+                  )
+                )
+              }
+            </div>
+          </div> : null
+        }
+      </div>
     </div>
   );
 }
